42d9b7a38ef0261e349a6172b98a685bce9cdbf6
Yaroslav Illyashenko <y.illyashenko@prismsolutions.ru>
2020-07-18 20:52:35 +0300

    feat: install HtmlWebpackPlugin and implement build script injection into existing index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,7 +10,13 @@ module.exports = {
     filename: "build.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
     rules: [{ test: /\.css$/, use: ["file-loader"] }],
   },
